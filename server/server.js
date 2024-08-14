d7501f69769656f60dcc66eda18c1e1ee097ad78
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-08-14 19:44:33 +0700

    feat(api): connect to mongo

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authroutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/db.js";
 dotenv.config();
+
+  connectToMongoDB()
 
 const app = express();
 app.get("/", (req, res) => {
