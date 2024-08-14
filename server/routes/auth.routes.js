123a4bab939cccab9c3722a22c2cf5ce8379ac1a
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-08-14 19:04:50 +0700

    feeat(api): auth endpoint

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,17 +1,12 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
-router.post("/signup", (req,res)=>{
-    res.json("signup routes")
-});
+router.post("/signup", signup);
 
-router.post("/login", (req,res)=>{
-    res.json("signup login")
+// router.post("/login", login);
 
-})
-router.post("/logout", (req,res)=>{
-    res.json("signup logout")
-
-})
+// router.post("/logout", logout);
 
 export default router;
